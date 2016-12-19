4647da20272654deebd57583ce7e03caf8de90be
thegodofsleep215 <thomas.1379@osu.edu>
2016-12-19 10:43:27 -0500

    Initial commit

---
+++ Nu.GeneticAlgorithm/GeneticAlgorithm.fs
@@ -0,0 +1,41 @@
+﻿namespace Nu.GeneticAlgorithm
+
+type GeneticAlgorithm<'a>(population : 'a array, 
+                            fitnessFunction : 'a -> double,
+                            crossoverFunction : 'a -> 'a -> 'a, 
+                            mutationFunction : 'a -> 'a,
+                            mutationChance : double ) = 
+    let mutable currentPopulation : ('a * double) array = population |> Array.map (fun x-> x, fitnessFunction(x))
+    let rand : System.Random = new System.Random()
+    let mutable lastBest = -2147483647.0
+    let popSize = currentPopulation.Length
+
+    member this.Run() : 'a =
+        while(not this.IsDone) do
+            let newPopulation = Async.Parallel [ for i in 0 .. currentPopulation.Length - 1 -> async { return this.ProcessNewChild(this.DoCrossover(currentPopulation)) } ] |> Async.RunSynchronously
+            currentPopulation <- this.Cull(popSize, Array.append newPopulation currentPopulation)
+        fst (currentPopulation |> Array.maxBy(fun x -> snd x))
+
+    member this.RandomSelection(population : ('a * double) array) : 'a = 
+        fst population.[rand.Next(0, population.Length)]
+
+    member this.DoCrossover(population : ('a * double) array) : 'a = 
+        let x = this.RandomSelection(population)
+        let y = this.RandomSelection(population)
+        crossoverFunction x y
+
+    member this.ProcessNewChild(child: 'a) : ('a * double) = 
+        let mc = rand.NextDouble()
+        let result = if mc <= mutationChance then mutationFunction(child) else child
+        result, fitnessFunction(result)
+
+    member this.AverageFitness (population : ('a * double) array) : double =
+            (Array.fold (fun acc x -> acc + snd x) 0.0 population)/double(population.Length)
+
+    member this.Cull(size : int, population: ('a * double) array) : ('a * double) array =
+        population |> Array.sortByDescending (fun (_, x) -> x) |> Array.take popSize
+
+    member this.IsDone : bool = 
+        let average = this.AverageFitness(currentPopulation)
+        let goodOnes : double = double((currentPopulation |> Array.filter (fun (_, x) -> System.Math.Round(x,9) >= System.Math.Round(average,9))).Length)
+        goodOnes/double(currentPopulation.Length) >= 0.9
---
+++ Nu.KnapSack/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using FSharpFuncUtil;
+using Microsoft.FSharp.Core;
+using Nu.GeneticAlgorithm;
+
+namespace Nu.KnapSack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxWeight = 20.0;
+            var baseItems = new[]
+            {
+                new KnapSack.Item(10, 5, false),
+                new KnapSack.Item(5, 2.5, false),
+                new KnapSack.Item(5, 2.5, false),
+                new KnapSack.Item(1, 10, false),
+                new KnapSack.Item(1, 10, false),
+                new KnapSack.Item(3, 7, false),
+                new KnapSack.Item(23, 4, false),
+                new KnapSack.Item(3, 1, false),
+                new KnapSack.Item(15, 19, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(3, 7, false),
+                new KnapSack.Item(23, 4, false),
+                new KnapSack.Item(3, 1, false),
+                new KnapSack.Item(15, 19, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(3, 7, false),
+                new KnapSack.Item(23, 4, false),
+                new KnapSack.Item(3, 1, false),
+                new KnapSack.Item(15, 19, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+                new KnapSack.Item(0, 10, false),
+            };
+            var pop = Enumerable.Repeat(0, 200).Select(x => KnapSack.Randomize(maxWeight, baseItems)).ToArray();
+            var fitness = FuncConvert.ToFSharpFunc<KnapSack, double>(KnapSack.Fitness);
+            var crossover = new Func<KnapSack, KnapSack, KnapSack>(KnapSack.Crossover).ToFSharpFunc();
+            var mutation = FuncConvert.ToFSharpFunc<KnapSack, KnapSack>(KnapSack.Mutate);
+            var ga = new GeneticAlgorithm<KnapSack>(pop, fitness, crossover, mutation, .05);
+            var result = ga.Run();
+            Console.WriteLine($"Best Fitness: {KnapSack.Fitness(result)} Weight: {result.Weight}");
+            Console.ReadKey();
+        }
+    }
+
+
+    class KnapSack
+    {
+        private static readonly Random rand = new Random();
+        public static KnapSack Randomize(double maxWeight, Item[] contents)
+        {
+            var newConetents = contents.Select(x => new Item(x.Value, x.Weight, false)).ToArray();
+            var result = new KnapSack(maxWeight, newConetents);
+            while (true)
+            {
+                var smallEnough = result.Contents.Where(x => result.MaxWeight >= (result.Weight + x.Weight) && !x.InSack).ToArray();
+                if (!smallEnough.Any()) break;
+                var i = rand.Next(0, smallEnough.Length);
+                smallEnough[i].InSack = true;
+            }
+            return result;
+        }
+
+        public static KnapSack Crossover(KnapSack a, KnapSack b)
+        {
+            return new KnapSack(a.MaxWeight, Enumerable.Range(0, a.Contents.Length).Select(i => rand.Next(0, 2) == 0 ? a.Contents[i] : b.Contents[i]).ToArray());
+        }
+
+        public static double Fitness(KnapSack a)
+        {
+            if (a.Weight > a.MaxWeight)
+            {
+                return 0;
+            }
+            return a.Value;
+        }
+
+        public static KnapSack Mutate(KnapSack a)
+        {
+            var smallEnough = a.Contents.Where(x => !x.InSack && x.Weight + a.Weight <= a.MaxWeight).ToArray();
+            if (smallEnough.Any())
+            {
+                smallEnough[rand.Next(0, smallEnough.Length)].InSack = true;
+            }
+            else
+            {
+                var inSack = a.Contents.Where(x => x.InSack).ToArray();
+                if (inSack.Any())
+                {
+                    inSack[rand.Next(0, inSack.Length)].InSack = false;
+                }
+            }
+            return a;
+        }
+
+
+        public class Item
+        {
+            public Item(double value, double weight, bool inSack)
+            {
+                Value = value;
+                Weight = weight;
+                InSack = inSack;
+            }
+
+            public double Value { get; set; }
+
+            public double Weight { get; set; }
+
+            public bool InSack { get; set; }
+
+            public override string ToString()
+            {
+                return $"Value: {Value} Weight:{Weight} InSack:{InSack}";
+            }
+        }
+
+
+        public double Value
+        {
+            get { return Contents.Sum(x => x.InSack ? x.Value : 0); }
+        }
+
+        public double Weight
+        {
+            get { return Contents.Sum(x => x.InSack ? x.Weight : 0); }
+        }
+
+        public double MaxWeight { get; set; }
+
+        public Item[] Contents { get; }
+
+        public KnapSack(double maxWeight, Item[] contents)
+        {
+            MaxWeight = maxWeight;
+            Contents = contents;
+        }
+
+        public override string ToString()
+        {
+            return $"Fitness: {Fitness(this)} Value: {Value} Weight: {Weight}";
+        }
+    }
+}
